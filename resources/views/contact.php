fe89661cbb3cdb964c072c5e289d3ec67bd8c5c4
Niculae Niculae <niculaenick@gmail.com>
2018-11-09 15:47:22 +0100

    Basic routing

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Nicu</title>
+    <title>Contact</title>
 </head>
 <body>
-    <h1>Laravel Nicu</h1>
-    <p><a href="/contact">Contact</a></p>
+    <h1>Contact</h1>
 </body>
 </html>
